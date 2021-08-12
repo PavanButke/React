e9ca8499bc35f3f347a3728dc7583fd97bd66976
PavanButke <butkepavan55@gmail.com>
2021-08-12 17:07:08 +0530

    movies app

--- movie-api/server.js
+++ movie-api/server.js
@@ -1,9 +1,11 @@
 let express = require("express");
+let cors = require("cors")
 
 let data = require("./data.json");
 
 //ek naya server bnade but ye sirf create krti use chalu nhi krti
 let server = express();
+server.use(cors())
 
 server.get("/movies", function (req, res) {
   res.json(data);
@@ -33,6 +35,6 @@ server.get("/genre", function (req, res) {
 
 //ye line server ko shuru krdeti hai
 // ek port pr
-// server.listen(4000);
+server.listen(4000);
 
-// //  http://localhost:3000/movies
+//  http://localhost:3000/movies
